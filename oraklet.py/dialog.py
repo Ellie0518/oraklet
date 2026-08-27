53b51586bbc82ad748877e787f525e8cf243275b
Ellie Gustafsson Renling <ellren05@edu.umea.se>
2026-08-27 13:34:39 +0200

    lektion torsdag

---
+++ oraklet.py/dialog.py
@@ -0,0 +1,8 @@
+name = input("name?")
+ålder_text = input("hur gammal är du?")
+ålder = int(ålder_text)
+print(ålder)
+aktuellt_år = 2026
+ålder = akutellt_år-ålder_text
+print(f"Hej!{name} du är {16}år")
+      
