6fdcdb91fbdbd96eb0ca00ab4e312d965814088d
Ellie Gustafsson Renling <ellren05@edu.umea.se>
2026-09-03 11:16:22 +0200

    tors 3 sep

--- oraklet.py/slump.py
+++ oraklet.py/slump.py
@@ -1,15 +1,13 @@
 import random
 
-ditt_nummer = random.randint(1, 4)
-datorns_nummer = random.randint(1, 4) 
+ditt_nummer = random.randint(1, 8)
+datorns_nummer = random.randint(1, 8) 
 
 namn = input("Vad heter du?")
 gissa = input("Vad är din gissning?")
 
-print(f"Du gissa: {ditt_nummer}")
-print(f"Datorn gissa: {datorns_nummer}")
-
-if ditt_nummer: int(total = random.randint(1, 4))
+if ditt_nummer: int(ditt_nummer > datorns_nummer)
 print("Rätt gissat!")
-if datorns_nummer: int(total = random.randint(1, 4))
-print("Tyvärr datorn gissa rätt.")
+
+else datorns_nummer: int(datorns_nummer > ditt_nummer)
+print("tyvärr datorn vann.")
